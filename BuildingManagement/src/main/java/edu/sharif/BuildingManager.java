65d2dcb4400c0e64745717aa72589fdfc5aa940f
mohammad <maghaie14@yahoo.com>
2025-08-29 21:27:22 +0330

    implement method to pass tests

--- BuildingManagement/src/main/java/edu/sharif/BuildingManager.java
+++ BuildingManagement/src/main/java/edu/sharif/BuildingManager.java
@@ -2,35 +2,47 @@ package edu.sharif;
 
 import edu.sharif.device.Device;
 import edu.sharif.pricePolicy.PricePolicy;
+import edu.sharif.pricePolicy.StandardPricePolicy;
+import edu.sharif.systemState.ActiveSystemState;
 import edu.sharif.systemState.SystemState;
 
 import java.util.List;
 
 public class BuildingManager {
 
-    public BuildingManager(List<Device> devices) {
+    private SystemState systemState;
+    private PricePolicy pricePolicy;
+    private final List<Device> devices;
 
+    public BuildingManager(List<Device> devices) {
+        this.devices = List.copyOf(devices);
+        systemState = new ActiveSystemState(this);
+        pricePolicy = StandardPricePolicy.INSTANCE;
     }
 
     public void setPricePolicy(PricePolicy pricePolicy) {
+        this.pricePolicy = pricePolicy;
     }
 
     public void setSystemState(SystemState systemState) {
+        this.systemState = systemState;
     }
 
     public SystemState getSystemState() {
-        return null;
+        return this.systemState;
     }
 
     public int totalPrice() {
-        return 0;
+        var activeDevice = systemState.activeDevices();
+        int energy = activeDevice.stream().mapToInt(Device::energyConsumption).sum();
+        return pricePolicy.price(energy);
     }
 
     public int simulate(int energy) {
-        return 0;
+        return pricePolicy.price(energy);
     }
 
     public List<Device> getDevices() {
-        return null;
+        return devices;
     }
 }
